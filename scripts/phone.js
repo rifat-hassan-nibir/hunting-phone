80609382ecafb232fd7a580bddfadfcf3f7800ff
rifat-hassan-nibir <rifathassannibir2@gmail.com>
2024-02-28 11:36:35 +0600

    finished till search bar

--- scripts/phone.js
+++ scripts/phone.js
@@ -1,1 +1,14 @@
-console.log("hellog");
+const loadPhones = async () => {
+  const response = await fetch("https://openapi.programming-hero.com/api/phones?search=iphone");
+  const data = await response.json();
+  const phones = data.data;
+  displayPhones(phones);
+};
+
+const displayPhones = (phones) => {
+  phones.forEach((phone) => {
+    console.log(phone);
+  });
+};
+
+loadPhones();
